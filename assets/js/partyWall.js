6ffa12ffda5a665c1720d5f23c008184a547739b
Johan Groenen <johan.groenen+github@gmail.com>
2014-12-28 20:46:17 +0000

    small update

--- assets/js/partyWall.js
+++ assets/js/partyWall.js
@@ -1,21 +1,26 @@
 (function () {
     
     var settings = {
-        tile: {
-            height: 400, // in pixels
-            width: 400,  // in pixels
-            image: 'tegeltje.png' // located in assets/img
+            tile: {
+                height: 400, // in pixels
+                width: 400,  // in pixels
+                image: 'tegeltje_400.png' // located in assets/img
+            },
+            wall: {
+                height: 100, // in tiles
+                width: 100   // in tiles
+            },
+            scroll: {
+                speed: 500 // pixels per second
+            },
+            display: {
+                time: 5000 // in miliseconds
+            }
         },
-        wall: {
-            height: 100, // in tiles
-            width: 100   // in tiles
-        },
-        scroll: {
-            speed: 500 // pixels per second
-        }
-    };
+        cl = {x: 0, y: 0}; // current location in pixels
     
     // Setting up the theme.
+    //FIXME need to set some themes here
     $('body').css({
         width: settings.wall.width * settings.tile.width,
         height: settings.wall.height * settings.tile.height,
@@ -23,13 +28,52 @@
         backgroundSize: settings.tile.width + 'px ' + settings.tile.height + 'px'
     });
     
-    // Go to a location.
-    var cl = {x: 0, y: 0}; // current location
-    var nl = {x: 10000, y: 10000}; // new location
-    var dx = nl.x - cl.x;
-    var dy = nl.y - cl.y;
-    $('html, body').animate({
-        scrollTop: nl.x,
-        scrollLeft: nl.y
-    }, Math.floor(Math.sqrt(dx * dx + dy * dy) * 1000 / settings.scroll.speed));
+    /**
+     * Animates moving from the current location to the next location.
+     */
+    function animateToNextLocation(nl) {
+        console.log('animating to location', nl);
+        var dx = nl.x - cl.x;
+        var dy = nl.y - cl.y;
+        $('body').animate(
+            {
+                scrollTop: nl.x,
+                scrollLeft: nl.y
+            },
+            Math.floor(
+                Math.sqrt(dx * dx + dy * dy) * 1000 /
+                settings.scroll.speed
+            ),
+            function () {
+                setTimeout(goToNextLocation, settings.display.time);
+            }
+        );
+        cl = nl;
+    }
+    
+    /**
+     * Chooses the next location.
+     */
+    function getNextLocation() {
+        var nl;
+        do {
+            //FIXME choose from content locations
+            //FIXME center on content location
+            nl = {
+                x: Math.floor(Math.random() * settings.wall.width) * settings.tile.width,
+                y: Math.floor(Math.random() * settings.wall.height) * settings.tile.height
+            };
+        } while (cl.x === nl.x || cl.y === nl.y); // Straight lines are boring.
+        return nl;
+    }
+    
+    /**
+     * Chooses the next location and animates moving there.
+     */
+    function goToNextLocation() {
+        animateToNextLocation(getNextLocation());
+    }
+    
+    // Start!
+    goToNextLocation();
 }());
